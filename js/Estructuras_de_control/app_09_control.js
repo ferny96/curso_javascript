cb869a90fb7301c789a451daf7fdd7f3034f4940
Fernando Rodriguez <fernandounah.rodriguez@gmail.com>
2021-12-23 18:44:25 -0600

    Actualizando curso

--- js/Estructuras_de_control/app_09_control.js
+++ js/Estructuras_de_control/app_09_control.js
@@ -1,6 +1,10 @@
 //Operador TERNARIO
-const autenticado = false;
-const puedePagar = true;
+//El operador ternario es parecido al if donde el signo ? sería la parte del if o verdadero mientras que el
+//signo de : sería la parte del false
+
+const autenticado = true;
+const puedePagar = false;
+
 console.log(autenticado ? "Si autenticado" : "No autenticado");
 
 //REVISAR DOS CONDICIONES
@@ -8,3 +12,7 @@ console.log(autenticado ? puedePagar ? "Si esta autenticado y puede pagar" : "Si
 
 
 //ANIDADOS CON EL OPERADOR TERNARIO
+//Parahacer un ternario anidado la sintaxis es la siguiente
+// condicion1 ? condicion2 ? se cumple la condición1 y condicion2 : no se cumple la condición2 : no se cumple la condicion1
+
+console.log(autenticado ? puedePagar ? "Esta autenticado y puede pagar" : "Sí auntenticado, no puede pagar" :"No autenticado");
